b0b60ab0e113632031568639a42e44bea2ace54e
Dan Davis <dan@danizen.net>
2021-02-21 22:59:10 -0500

    Adding styles

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,24 @@ gulp.task('genlore', function(cb) {
     spawn('python', ['genlore.py', '-o', 'src/js/lore.js'], PIPE).on('close', cb);
 });
 
+gulp.task('styles', function() {
+    return gulp.src([
+        'src/css/*.css',
+    ])
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('./dest/css'));
+});
+
 gulp.task('scripts', function() {
     return gulp.src([
-            'node_modules/leaflet/dist/leaflet.js',
-            'node_modules/leaflet-draw/dist/leaflet.draw.js',
-            'src/js/*.js',
-        ])
+        'node_modules/leaflet/dist/leaflet.js',
+        'node_modules/leaflet-draw/dist/leaflet.draw.js',
+        'src/js/*.js',
+    ])
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest('./dest/js'));
 });
 
-gulp.task('default', gulp.series('genlore', 'scripts'))
+gulp.task('default',
+    gulp.series('styles', 'genlore', 'scripts')
+);
